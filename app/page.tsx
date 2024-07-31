9d4301a9cd7ed9ef82f095d5801b2a371c99cec1
Michael Gwynne <michael@votemike.co.uk>
2024-07-31 10:25:18 +0100

    Example

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,22 @@
+import type {Metadata} from "next";
+
 /** Add your relevant code here for the issue to reproduce */
+export async function generateMetadata(): Promise<Metadata> {
+  const ukWebsite = new URL('/', 'https://example.com');
+  const usWebsite = new URL('/us', 'https://example.com');
+
+  console.log({
+    ukWebsite: ukWebsite,
+    ukWebsiteToString: ukWebsite.toString(),
+    usWebsite: usWebsite,
+    usWebsiteToString: usWebsite.toString()
+  });
+
+  return {
+    alternates: { languages: {'en-GB': ukWebsite, 'en-US': usWebsite } }
+  };
+}
+
 export default function Home() {
   return null;
 }
